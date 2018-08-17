4736944cf5d9ed9e2a12cd99fdc087a060f5573a
monalisagreen <42207546+monalisagreen@users.noreply.github.com>
2018-08-17 11:42:36 +0530

    Add files via upload

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+//first include the library
+#include<iostream>
 using namespace std;
-int main(){
-           cout<<"hello";
-           return 0;
+// write the main function
+int main (){
+//declaring the variables
+int a;
+float b;
+char c;
+bool d;
+//print the results
+cout << "size of int is" << sizeof(a) << endl;
+cout << "size of float is" << sizeof(b) << endl;
+cout << "size of char is" << sizeof(c) << endl;
+cout << "size of bool is" << sizeof(d) << endl;
+return 0;
 }
+
