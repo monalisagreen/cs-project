6a16885460406363a499e214677245024c220a82
monalisagreen <42207546+monalisagreen@users.noreply.github.com>
2018-08-17 19:45:39 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,28 @@
+//library
+#include <iostream>
+using namespace std;
+int main(){
+	//declaring variables
+	int a;
+	int b;
+	int sum;
+	int diff;
+	int multi;
+	int divi;
+	int mod;
+	//intialising variables
+	a=6;
+	b=3;
+	sum= a + b;
+	diff= a - b;
+	multi= a * b;
+	divi= a / b;
+	//displaying values 
+	cout<<"sum= "<<sum<<endl;
+	cout<<"difference= "<<diff<<endl;
+	cout<<"multiplication= "<<multi<<endl;
+	cout<<"division= "<<divi<<endl;
+	cout<<"modulus= "<<mod<<endl;
+return 0;
+}
+	
