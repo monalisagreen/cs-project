2580a4ece955ceaa5dc9aafdb84cda081fd7cad9
monalisagreen <42207546+monalisagreen@users.noreply.github.com>
2018-08-22 12:00:49 +0530

    Add files via upload

---
+++ lab4_q5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int ang1,ang2,ang3;
+cout << "find the third angle of the triangle";
+cout << "the 1st angle of triangle";
+cin >> ang1;
+cout << " the 2nd angle of triangle";
+cin >> ang2;
+ang3=180-(ang1+ang2);
+cout <<" the 3rd angle of the triangle is " << ang3 << endl;
+cout << endl;
+return 0;
+}
