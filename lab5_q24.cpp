5e634c602000c0d7deb3b21c8a02c4f57bbaef50
monalisagreen <42207546+monalisagreen@users.noreply.github.com>
2018-09-05 20:25:04 +0530

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,14 @@
+// program to print all alphabets from a to z 
+#include <iostream>
+using namespace std;
+int main(){
+	//declaring variables
+	char aa;
+	cout<<"the alphabets from a to z are"<<endl;
+	aa = 'a';
+	while(aa<='z'){
+		cout<< aa;
+		aa = aa + 1;
+		}
+return 0;
+}
