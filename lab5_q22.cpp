177580dd8c07df86297130cd13d0b2d36c05d1a4
monalisagreen <42207546+monalisagreen@users.noreply.github.com>
2018-09-05 20:55:52 +0530

    Add files via upload

---
+++ lab5_q22.cpp
@@ -0,0 +1,14 @@
+//To print all natural numbers from 1 to n
+#include <iostream>
+using namespace std;
+int main(){
+int a,n;
+cout<< "enter the n";
+cin>>n;
+a = 0;
+while(a<=n){
+a = a + 1;
+cout<< a <<endl;
+}
+return 0;
+}
